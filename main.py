b3f7fd0fb338e51ada78e07d5aa61b1f5c2d2f67
Serhii Denysov <sergeydenisovcad@gmail.com>
2016-05-13 18:23:24 +0300

    round check function first implementation

--- main.py
+++ main.py
@@ -17,6 +17,7 @@
 import os
 import webapp2
 import jinja2
+import logging
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -33,15 +34,31 @@ class MainHandler(webapp2.RequestHandler):
                 [Agent(True), Agent(False), Agent(True), Agent(False), Agent(True)],
                 [Agent(True), Agent(False), Agent(True), Agent(False), Agent(True)],
                 [Agent(True), Agent(False), Agent(True), Agent(False), Agent(True)]
-            ]
+            ],
+            'temp': Agent(False, 3, 4).getSurroundedAgents()
         }
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
 
 
 class Agent:
-    def __init__(self, isAlive):
+    def __init__(self, isAlive, coordX=0, coordY=0):
         self.isAlive = isAlive
+        self.coordX = coordX
+        self.coordY = coordY
+
+    def __str__(self):
+        return "Agent:\nisAlive = %s\ncoordX = %s\ncoordY = %s" % (self.isAlive, self.coordX, self.coordY)
+
+    def getSurroundedAgents(self):
+        surroundedAgents = list()
+        # i = 0
+        if self.coordX - 1 >= 0:
+            surroundedAgents.append(Agent(True, self.coordX - 1, self.coordY))
+            # i=i+1
+        print surroundedAgents
+        logging.debug(surroundedAgents)
+        return surroundedAgents
 
 
 app = webapp2.WSGIApplication([
